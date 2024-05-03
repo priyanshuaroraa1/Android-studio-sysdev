ef414d8cf32e80c6bcea06a0cee0afc35372b663
PriyanshuAroraa <s364732@oslomet.no>
2024-05-03 12:54:31 +0200

    Fått til å vise varsel når onboarding er ferdig og når brukeren legger til badeplass i favoritter.

--- app/src/main/java/no/uio/ifi/in2000/martirhe/appsolution/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/martirhe/appsolution/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -94,10 +95,14 @@ class MainActivity : ComponentActivity() {
                     LaunchedEffect(Unit) {
                         if (!isNetworkAvailable()) {
                             scope.launch {
-                                snackbarHostState.showSnackbar(
-                                    "Ingen internettforbindelse. Noen funksjoner vil ikke være tilgjengelige",
+                                val result = snackbarHostState.showSnackbar(
+                                    message = "Ingen internettforbindelse. Noen funksjoner vil ikke være tilgjengelige",
+                                    actionLabel = "Fortsett",
                                     duration = SnackbarDuration.Long
                                 )
+                                if (result == SnackbarResult.ActionPerformed) {
+                                    //Lukker snackbar
+                                }
                             }
                         }
                     }
