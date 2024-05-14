29115be9b14eb2380ee52ff1bfd888b6843e447a
berndklare <bernd@klare.no>
2024-05-14 16:51:07 +0200

    Rettet opp i noen feil i navigasjon. Navigerer fortsatt dobbelt til HomeScreen første gang etter onboarding

--- app/src/main/java/no/uio/ifi/in2000/martirhe/appsolution/ui/navigation/navbar/BottomNavBar.kt
+++ app/src/main/java/no/uio/ifi/in2000/martirhe/appsolution/ui/navigation/navbar/BottomNavBar.kt
@@ -95,20 +95,17 @@ fun BottomNavBar(
                     selected = currentRoute == item.route,
                     onClick = {
 
-                        navController.navigate(item.route) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = true
-                            }
-                            // Avoid multiple copies of the same destination when
-                            // reselecting the same item
-                            launchSingleTop = true
-                            // Restore state when reselecting a previously selected item
-                            restoreState = true
-                        }
+                        if (currentDestination?.route != item.route) {
+                            navController.navigate(item.route) {
+                                // Only pop up to the start destination if we're actually navigating to a new screen.
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
 
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
                     }
                 )
             }
